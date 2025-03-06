2b648876a6c14fbb79c84c59515d8a7302b27c72
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-03-06 15:12:41 +0200

    comment out modules in the model temporarily

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Event < ApplicationRecord
-  include Modelable
-  include Identifiable
-  include Elasticsearch::Model
+  # include Modelable
+  # include Identifiable
+  # include Elasticsearch::Model
 end
