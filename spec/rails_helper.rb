ec01e8f9e132dada0ded4e5d329b832fdcd5a63a
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-03-06 16:05:20 +0200

    fix git action

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,9 +30,8 @@ require "rspec/rails"
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
+  # TODO: add this back to make specs work with db
   # ActiveRecord::Migration.maintain_test_schema!
-  puts "skipping ActiveRecord::Migration.maintain_test_schema! for now"
-  puts "add back when test db is setup"
 rescue ActiveRecord::PendingMigrationError => e
   abort(e.to_s.strip)
 end
@@ -45,7 +44,8 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # TODO: add this back to make specs work with db
+  # config.use_transactional_fixtures = true
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
