81d86b66e8d54514183b9a804ef6a6ab66c8aca0
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-03-18 11:37:02 +0200

    fix specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,11 +8,12 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 
 require "rspec/rails"
 
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  abort(e.to_s.strip)
-end
+# TODO: add this back when you create the new events database
+# begin
+#   ActiveRecord::Migration.maintain_test_schema!
+# rescue ActiveRecord::PendingMigrationError => e
+#   abort(e.to_s.strip)
+# end
 
 RSpec.configure do |config|
   config.fixture_paths = [
