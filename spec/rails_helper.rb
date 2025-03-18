5f3ebd0c3ec297eae17458e9447ffc2a116ac7e3
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-03-18 11:15:28 +0200

    attempt to skip db checks for specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,11 +8,11 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 
 require "rspec/rails"
 
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  abort(e.to_s.strip)
-end
+# begin
+#   ActiveRecord::Migration.maintain_test_schema!
+# rescue ActiveRecord::PendingMigrationError => e
+#   abort(e.to_s.strip)
+# end
 
 RSpec.configure do |config|
   config.fixture_paths = [
