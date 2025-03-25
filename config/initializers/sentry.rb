110e7320c2782f8633b4a2d081c3372abe829049
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-03-25 16:34:57 +0200

    fix environments

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
-  config.environments = ["stage", "production"]
+  config.enabled_environments = ["stage", "production"]
   config.dsn = ENV["SENTRY_DSN"]
   config.release = "events:" + Events::Application::VERSION
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   config.logger = Rails.application.config.lograge.logger
+  config.send_default_pii = true
 end
 # Raven.configure do |config|
 #   config.environments = ["stage", "production"]
