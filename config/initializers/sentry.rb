746fbd2d5d183a1fe86a61bc978e0e74ee66b28c
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-03-25 16:22:42 +0200

    change to suggested sentry gems

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
-Raven.configure do |config|
+Sentry.init do |config|
   config.environments = ["stage", "production"]
   config.dsn = ENV["SENTRY_DSN"]
   config.release = "events:" + Events::Application::VERSION
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   config.logger = Rails.application.config.lograge.logger
 end
+# Raven.configure do |config|
+#   config.environments = ["stage", "production"]
+#   config.dsn = ENV["SENTRY_DSN"]
+#   config.release = "events:" + Events::Application::VERSION
+#   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+#   config.logger = Rails.application.config.lograge.logger
+# end
