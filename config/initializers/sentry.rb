2a7c512828dc3ed64c0a51f3ca2a933c18552790
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-03-25 16:47:50 +0200

    remove sanitize config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,7 +4,6 @@ Sentry.init do |config|
   config.enabled_environments = ["stage", "production"]
   config.dsn = ENV["SENTRY_DSN"]
   config.release = "events:" + Events::Application::VERSION
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   config.logger = Rails.application.config.lograge.logger
   config.send_default_pii = true
 
