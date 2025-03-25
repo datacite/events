cf0e57665be8a194e4cd09573de2c242bdcff4de
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-03-25 16:46:10 +0200

    fix filter params

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -7,6 +7,24 @@ Sentry.init do |config|
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   config.logger = Rails.application.config.lograge.logger
   config.send_default_pii = true
+
+  filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
+
+  config.before_send = lambda do |event, _hint|
+    if event.extra
+      event.extra = filter.filter(event.extra)
+    end
+
+    if event.user
+      event.user = filter.filter(event.user)
+    end
+
+    if event.contexts
+      event.contexts = filter.filter(event.contexts)
+    end
+
+    event
+  end
 end
 # Raven.configure do |config|
 #   config.environments = ["stage", "production"]
