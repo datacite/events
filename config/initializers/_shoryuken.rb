e65527ce2a5fa3fda87b721abdbb0a6ade626c62
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-03-26 16:21:04 +0200

    remove all sentry raven references

--- config/initializers/_shoryuken.rb
+++ config/initializers/_shoryuken.rb
@@ -8,19 +8,4 @@ if Rails.env.development?
   })
 end
 
-# Shoryuken middleware to capture worker errors and send them on to Sentry.io
-# module Shoryuken
-#   module Middleware
-#     module Server
-#       class RavenReporter
-#         def call(_worker_instance, queue, _sqs_msg, body, &block)
-#           tags = { job: body["job_class"], queue: queue }
-#           context = { message: body }
-#           Raven.capture(tags: tags, extra: context, &block)
-#         end
-#       end
-#     end
-#   end
-# end
-
 Shoryuken.active_job_queue_name_prefixing = true
