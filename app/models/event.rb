67365c5b218d23edd7f9a5899e6bc0b5fdb4bb75
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-04-14 11:57:52 +0200

    create initial event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,33 +1,39 @@
-# frozen_string_literal: true
-
-class Event
-  include ActiveModel::Model
-  include ActiveModel::Attributes
+class Event < ApplicationRecord
   # include Modelable
   # include Identifiable
   # include Elasticsearch::Model
 
-  attribute :id, :integer
-  attribute :uuid, :string
-  attribute :subj_id, :string
-  attribute :obj_id, :string
+  # Attributes
+  attribute :uuid, :text
+  attribute :subj_id, :text
+  attribute :obj_id, :text
   attribute :source_id, :string
   attribute :aasm_state, :string # could we remove this
   attribute :state_event, :string # could we remove this
-  attribute :callback, :string
-  attribute :error_messages, :string
-  attribute :source_token, :string
+  attribute :callback, :text
+  attribute :error_messages, :text
+  attribute :source_token, :text
   attribute :created_at, :datetime
   attribute :updated_at, :datetime
-  attribute :indexed_at, :datetime
+  attribute :indexed_at, :datetime, default: -> { Time.zone.at(0) }
   attribute :occurred_at, :datetime
-  attribute :message_action, :string # how is this set?
-  attribute :subj, :string
-  attribute :obj, :string
-  attribute :total, :integer
+  attribute :message_action, :string, default: "create" # how is this set?
+  attribute :subj, :text
+  attribute :obj, :text
+  attribute :total, :integer, default: 1
   attribute :license, :string
-  attribute :source_doi, :string
-  attribute :target_doi, :string
+  attribute :source_doi, :text
+  attribute :target_doi, :text
   attribute :source_relation_type_id, :string
   attribute :target_relation_type_id, :string
+
+  # Validations
+  validates :uuid, presence: true, uniqueness: {
+    case_sensitive: false,
+    length: { maximum: 36 } }
+  validates :subj_id, presence: true
+  validates :message_action, presence: true, length: { maximum: 191 }
+  validates :created_at, presence: true
+  validates :updated_at, presence: true
+  validates :indexed_at, presence: true
 end
