35fb0afd3026c295196a915df2fef77221175610
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-05-13 17:02:27 +0200

    Fix modules

--- app/utilities/doi_utilities.rb
+++ app/utilities/doi_utilities.rb
@@ -1,23 +1,25 @@
 # frozen_string_literal: true
 
 module DoiUtilities
-  def self.normalize_doi(url)
-    doi =
-      Array(%r{\A(?:(http|https):/(/)?(dx\.)?(doi.org|handle.test.datacite.org)/)?(doi:)?(10\.\d{4,5}/.+)\z}
-      .match(doi)).last
+  class << self
+    def normalize_doi(url)
+      doi =
+        Array(%r{\A(?:(http|https):/(/)?(dx\.)?(doi.org|handle.test.datacite.org)/)?(doi:)?(10\.\d{4,5}/.+)\z}
+        .match(doi)).last
 
-    doi = doi.delete("\u200B").downcase if doi.present?
+      doi = doi.delete("\u200B").downcase if doi.present?
 
-    "https://doi.org/#{doi}" if doi.present?
-  end
+      "https://doi.org/#{doi}" if doi.present?
+    end
 
-  def self.uppercase_doi_from_url(url)
-    if %r{\A(?:(http|https)://(dx\.)?(doi.org|handle.test.datacite.org)/)?(doi:)?(10\.\d{4,5}/.+)\z}
-        .match?(url)
+    def uppercase_doi_from_url(url)
+      if %r{\A(?:(http|https)://(dx\.)?(doi.org|handle.test.datacite.org)/)?(doi:)?(10\.\d{4,5}/.+)\z}
+          .match?(url)
 
-      uri = Addressable::URI.parse(url)
+        uri = Addressable::URI.parse(url)
 
-      uri.path.gsub(%r{^/}, "").upcase
+        uri.path.gsub(%r{^/}, "").upcase
+      end
     end
   end
 end
