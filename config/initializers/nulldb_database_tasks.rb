643d9c3f0fccb78c70191bf9ea1278f54b1822c2
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-05-19 13:23:51 +0200

    Fix schema checks

--- config/initializers/nulldb_database_tasks.rb
+++ config/initializers/nulldb_database_tasks.rb
@@ -2,6 +2,15 @@
 
 # Patch DatabaseTasks for nulldb to prevent schema loading attempts
 if Rails.env.test? && defined?(ActiveRecord::Tasks::DatabaseTasks)
+  # module ActiveRecord
+  #   module Tasks
+  #     class NullDBDatabaseTasks
+  #       def load_schema(*)
+  #         # no-op
+  #       end
+  #     end
+  #   end
+  # end
   module ActiveRecord
     module Tasks
       class NullDBDatabaseTasks
@@ -15,12 +24,20 @@ if Rails.env.test? && defined?(ActiveRecord::Tasks::DatabaseTasks)
   ActiveRecord::Tasks::DatabaseTasks.register_task(/nulldb/, ActiveRecord::Tasks::NullDBDatabaseTasks)
 end
 
-if Rails.env.test?
-  module ActiveRecord
-    class Migration
-      class << self
-        def check_pending!; end
-      end
+# if Rails.env.test?
+#   module ActiveRecord
+#     class Migration
+#       class << self
+#         def check_pending!; end
+#       end
+#     end
+#   end
+# end
+
+module ActiveRecord
+  class Migration
+    class << self
+      def check_pending!; end
     end
   end
 end
