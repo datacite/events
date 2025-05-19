1595c85803875dfa3923a0c7c3a4b711ac86db39
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-05-19 13:32:12 +0200

    Revert database.yml change

--- config/initializers/nulldb_database_tasks.rb
+++ config/initializers/nulldb_database_tasks.rb
@@ -2,15 +2,6 @@
 
 # Patch DatabaseTasks for nulldb to prevent schema loading attempts
 if Rails.env.test? && defined?(ActiveRecord::Tasks::DatabaseTasks)
-  # module ActiveRecord
-  #   module Tasks
-  #     class NullDBDatabaseTasks
-  #       def load_schema(*)
-  #         # no-op
-  #       end
-  #     end
-  #   end
-  # end
   module ActiveRecord
     module Tasks
       class NullDBDatabaseTasks
@@ -24,20 +15,12 @@ if Rails.env.test? && defined?(ActiveRecord::Tasks::DatabaseTasks)
   ActiveRecord::Tasks::DatabaseTasks.register_task(/nulldb/, ActiveRecord::Tasks::NullDBDatabaseTasks)
 end
 
-# if Rails.env.test?
-#   module ActiveRecord
-#     class Migration
-#       class << self
-#         def check_pending!; end
-#       end
-#     end
-#   end
-# end
-
-module ActiveRecord
-  class Migration
-    class << self
-      def check_pending!; end
+if Rails.env.test?
+  module ActiveRecord
+    class Migration
+      class << self
+        def check_pending!; end
+      end
     end
   end
 end
