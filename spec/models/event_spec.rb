b825cc75e26f2eea2319502fd247c97bc242c824
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-05-20 17:08:28 +0200

    Add event model specs

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,5 +3,23 @@
 require "rails_helper"
 
 RSpec.describe(Event, type: :model) do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "includes the RelationTypeHandler" do
+    expect(described_class.ancestors).to(include(RelationTypeHandler))
+  end
+
+  it "includes the EventIndexHandler" do
+    expect(described_class.ancestors).to(include(EventIndexHandler))
+  end
+
+  it "includes the EventFromSqsCreator" do
+    expect(described_class.ancestors).to(include(EventFromSqsCreator))
+  end
+
+  it "includes the EventFromSqsUpdater" do
+    expect(described_class.ancestors).to(include(EventFromSqsUpdater))
+  end
+
+  it "includes the Elasticsearch::Model" do
+    expect(described_class.ancestors).to(include(Elasticsearch::Model))
+  end
 end
