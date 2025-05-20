06ed017f3613967b1dab40b81fb1ebeceadcc6c4
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-05-20 13:12:36 +0200

    Add doi utilities specs

--- spec/utlilities/doi_utilities_spec.rb
+++ spec/utlilities/doi_utilities_spec.rb
@@ -113,4 +113,55 @@ RSpec.describe(DoiUtilities) do
       expect(doi).to(eq("10.5281/ZENODO.1234567"))
     end
   end
+
+  describe "#doi_from_url" do
+    it "returns nil if url is invalid" do
+      url = "https://invalid.org/zenodo.invalid"
+      doi = described_class.doi_from_url(url)
+
+      expect(doi).to(be_nil)
+    end
+
+    it "returns a non-nil doi when passed a regular https doi url" do
+      url = "https://doi.org/10.5281/zenodo.1234567"
+      doi = described_class.doi_from_url(url)
+
+      expect(doi).to(eq("10.5281/zenodo.1234567"))
+    end
+
+    it "returns a non-nil doi when passed a regular http doi url" do
+      url = "http://doi.org/10.5281/zenodo.1234567"
+      doi = described_class.doi_from_url(url)
+
+      expect(doi).to(eq("10.5281/zenodo.1234567"))
+    end
+
+    it "returns a non-nil doi when passed a dx url" do
+      url = "https://dx.doi.org/10.5281/zenodo.1234567"
+      doi = described_class.doi_from_url(url)
+
+      expect(doi).to(eq("10.5281/zenodo.1234567"))
+    end
+
+    it "returns a non-nil doi when passed a handle test url" do
+      url = "https://handle.test.datacite.org/10.5281/zenodo.1234567"
+      doi = described_class.doi_from_url(url)
+
+      expect(doi).to(eq("10.5281/zenodo.1234567"))
+    end
+
+    it "returns a non-nil doi when passed a doi prefix url" do
+      url = "doi:10.5281/zenodo.1234567"
+      doi = described_class.doi_from_url(url)
+
+      expect(doi).to(eq("10.5281/zenodo.1234567"))
+    end
+
+    it "returns a non-nil doi when passed a bare doi" do
+      url = "10.5281/zenodo.1234567"
+      doi = described_class.doi_from_url(url)
+
+      expect(doi).to(eq("10.5281/zenodo.1234567"))
+    end
+  end
 end
