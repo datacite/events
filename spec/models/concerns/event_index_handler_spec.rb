7b5c63b7a95517249586170e6d6d07c1ec7e78ac
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-05-22 13:56:03 +0200

    Add event index handler specs

--- spec/models/concerns/event_index_handler_spec.rb
+++ spec/models/concerns/event_index_handler_spec.rb
@@ -7,70 +7,40 @@ RSpec.describe(EventIndexHandler, type: :concern) do
 
   describe ".subj_cache_key" do
     it "when subj_hash has 'dateModified' returns expected result" do
-      event = Event.new(
-        subj_id: "00.0000/zenodo.00000000",
-        subj: {
-          "@id" => "00.0000/zenodo.00000000",
-          "@type" => "Organization",
-          "name" => "DataCite",
-          "dateModified" => "2025-01-01 00:00:00",
-          "location" => {
-            "type" => "postalAddress",
-            "addressCountry" => "Germany",
-          },
-        }.to_json,
-      )
+      event = build(:event)
+      event.subj_id = "00.0000/zenodo.00000000"
+      event.subj = { "dateModified": "2025-01-01 00:00:00" }.to_json
 
-      expected = "objects/00.0000/zenodo.00000000-2025-01-01 00:00:00"
-
-      expect(event.subj_cache_key).to(eq(expected))
+      expect(event.subj_cache_key).to(eq("objects/00.0000/zenodo.00000000-2025-01-01 00:00:00"))
     end
 
     it "when subj_hash does not have a 'dateModified' returns expected result" do
       travel_to(Time.zone.parse("2025-01-01T00:00:00Z")) do
-        event = Event.new(
-          subj_id: "00.0000/zenodo.00000000",
-          subj: { "field": "value" }.to_json,
-        )
+        event = build(:event)
+        event.subj_id = "00.0000/zenodo.00000000"
+        event.subj = { "key": "value" }.to_json
 
-        expected = "objects/00.0000/zenodo.00000000-2025-01-01T00:00:00Z"
-
-        expect(event.subj_cache_key).to(eq(expected))
+        expect(event.subj_cache_key).to(eq("objects/00.0000/zenodo.00000000-2025-01-01T00:00:00Z"))
       end
     end
   end
 
   describe ".obj_cache_key" do
     it "when obj_hash has 'dateModified' returns expected result" do
-      event = Event.new(
-        obj_id: "00.0000/zenodo.00000000",
-        obj: {
-          "@id" => "00.0000/zenodo.00000000",
-          "@type" => "Organization",
-          "name" => "DataCite",
-          "dateModified" => "2025-01-01 00:00:00",
-          "location" => {
-            "type" => "postalAddress",
-            "addressCountry" => "Germany",
-          },
-        }.to_json,
-      )
+      event = build(:event)
+      event.obj_id = "00.0000/zenodo.00000000"
+      event.obj = { "dateModified": "2025-01-01 00:00:00" }.to_json
 
-      expected = "objects/00.0000/zenodo.00000000-2025-01-01 00:00:00"
-
-      expect(event.obj_cache_key).to(eq(expected))
+      expect(event.obj_cache_key).to(eq("objects/00.0000/zenodo.00000000-2025-01-01 00:00:00"))
     end
 
     it "when obj_hash does not have a 'dateModified' returns expected result" do
       travel_to(Time.zone.parse("2025-01-01T00:00:00Z")) do
-        event = Event.new(
-          obj_id: "00.0000/zenodo.00000000",
-          obj: { "field": "value" }.to_json,
-        )
+        event = build(:event)
+        event.obj_id = "00.0000/zenodo.00000000"
+        event.obj = { "key": "value" }.to_json
 
-        expected = "objects/00.0000/zenodo.00000000-2025-01-01T00:00:00Z"
-
-        expect(event.obj_cache_key).to(eq(expected))
+        expect(event.obj_cache_key).to(eq("objects/00.0000/zenodo.00000000-2025-01-01T00:00:00Z"))
       end
     end
   end
