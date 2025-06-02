c3ae7df5bad590668394c472cac2ebd4748540ec
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-06-02 16:10:02 +0200

    Add additional logging

--- vendor/docker/shoryuken.sh
+++ vendor/docker/shoryuken.sh
@@ -2,6 +2,6 @@
 cd /home/app/webapp
 exec 2>&1
 # Disabled with DISABLE_QUEUE_WORKER, start shoryuken
-if [ -n "$DISABLE_QUEUE_WORKER" = "true" ]; then
+if [ "$DISABLE_QUEUE_WORKER" = "true" ]; then
   exec /sbin/setuser app bundle exec shoryuken -R -C config/shoryuken.yml
 fi
