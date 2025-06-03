63ae0497a0487bc5a2c0e4a227f96fae6a6ee3e3
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-06-03 08:54:47 +0200

    Add additional logging

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,8 @@
 
 class EventsController < ApplicationController
   def index
+    Rails.logger.info("EventsController#index called")
+    Rails.logger.info("EventsController#index testing logging")
     message = { data: { action: "events#index" } }
 
     render(json: message)
