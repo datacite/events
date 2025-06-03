a66a8bf80361354342d0bee41ba4366bacff054c
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-06-03 13:30:55 +0200

    Fix logic error in shoryuken.sh

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,8 +2,6 @@
 
 class EventsController < ApplicationController
   def index
-    Rails.logger.info("EventsController#index called")
-    Rails.logger.info("EventsController#index testing logging")
     message = { data: { action: "events#index" } }
 
     render(json: message)
