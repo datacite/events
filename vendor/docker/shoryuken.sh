a434214e56a6324bbc85086a63534f79aaed59f5
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-06-03 14:31:44 +0200

    Add specs and comments

--- vendor/docker/shoryuken.sh
+++ vendor/docker/shoryuken.sh
@@ -7,5 +7,6 @@ exec 2>&1
 # Disble starting the queue worker when the environment variable DISABLE_QUEUE_WORKER is set to "true".
 # This value is set in the docker-compose.yml file
 if [ "$DISABLE_QUEUE_WORKER" != "true" ]; then
+  echo "Starting Shoryuken worker"
   exec /sbin/setuser app bundle exec shoryuken -R -C config/shoryuken.yml
 fi
