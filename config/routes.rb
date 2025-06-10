eafc8b2a49bb57ddfd02926259f9307409d769b8
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-06-10 14:02:49 +0200

    Fix heartbeat route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :heartbeat, only: [:index]
+  get "heartbeat", to: "heartbeat#index"
 
   root to: "heartbeat#index"
 end
