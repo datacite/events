136d8d51bb053d6dd7da2ccdb9e80962f0f3323a
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-06-10 11:56:35 +0200

    Remove the events controller

--- app/controllers/events_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class EventsController < ApplicationController
-  def index
-    message = { data: { action: "events#index" } }
-
-    render(json: message)
-  end
-end
