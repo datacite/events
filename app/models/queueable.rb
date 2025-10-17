2c87bd57d76c47190d1451fb3ffd450ef21f11e3
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-10-17 12:42:25 +0200

    Implement other doi job queuing logic

---
+++ app/models/queueable.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module Queueable
+  extend ActiveSupport::Concern
+
+  require "aws-sdk-sqs"
+
+  class_methods do
+    def send_events_other_doi_job_message(data)
+      send_message(data, shoryuken_class: "OtherDoiJobWorker", queue_name: "events_other_doi_job")
+    end
+
+    private
+
+    def send_message(body, options = {})
+      sqs = get_sqs_client
+      queue_name_prefix = ENV["SQS_PREFIX"].presence || Rails.env
+      queue_url = sqs.get_queue_url(queue_name: "#{queue_name_prefix}_#{options[:queue_name]}").queue_url
+
+      options = {
+        queue_url: queue_url,
+        message_attributes: {
+          "shoryuken_class" => {
+            string_value: options[:shoryuken_class],
+            data_type: "String",
+          },
+        },
+        message_body: body.to_json,
+      }
+
+      sqs.send_message(options)
+    rescue => error
+      Rails.logger.error("Failed to send message to #{queue_url}. #{error.inspect}.")
+    end
+
+    def get_sqs_client
+      if Rails.env.development?
+        Aws::SQS::Client.new(endpoint: ENV["AWS_ENDPOINT"])
+      else
+        Aws::SQS::Client.new
+      end
+    end
+  end
+end
