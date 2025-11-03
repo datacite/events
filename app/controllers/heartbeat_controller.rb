7aa230482ab6b34548e3d3774badfca983711bb7
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-11-03 17:07:09 +0200

    Add some testing code into the controller

--- app/controllers/heartbeat_controller.rb
+++ app/controllers/heartbeat_controller.rb
@@ -2,6 +2,7 @@
 
 class HeartbeatController < ApplicationController
   def index
+    SqsUtilities.send_events_other_doi_job_message({ subj_id: "fake_subj_id", obj_id: "fake_obj_id" })
     render(
       plain: "OK",
       status: :ok,
