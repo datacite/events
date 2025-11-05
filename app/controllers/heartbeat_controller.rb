696935fd08b0323698beb9dc4058721c17f7f1ae
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-11-05 10:54:30 +0200

    Remove testing code

--- app/controllers/heartbeat_controller.rb
+++ app/controllers/heartbeat_controller.rb
@@ -2,7 +2,6 @@
 
 class HeartbeatController < ApplicationController
   def index
-    SqsUtilities.send_events_other_doi_job_message({ subj_id: "10.13038/FOO.BAR", obj_id: "10.13038/BAR.FOO" })
     render(
       plain: "OK",
       status: :ok,
