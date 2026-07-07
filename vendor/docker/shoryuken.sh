152f65358d60abfa5c391b0b383a18caee9fbb19
Bryceson Laing <bklaing2@gmail.com>
2026-07-07 16:25:03 -0500

    fix typo

--- vendor/docker/shoryuken.sh
+++ vendor/docker/shoryuken.sh
@@ -4,7 +4,7 @@ cd /home/app/webapp
 # Redirect stdout and stderr to the same place 
 exec 2>&1
 
-# Disble starting the queue worker when the environment variable DISABLE_QUEUE_WORKER is set to "true".
+# Disable starting the queue worker when the environment variable DISABLE_QUEUE_WORKER is set to "true".
 # This value is set in the docker-compose.yml file
 if [ "$DISABLE_QUEUE_WORKER" != "true" ]; then
   echo "Starting Shoryuken worker"
